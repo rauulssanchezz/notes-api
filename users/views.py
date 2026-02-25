437394e6d384d8b09d7687dd7ebe127cb103e5dc
raul <rauulssanchezz@gmail.com>
2026-02-25 12:01:00 +0100

    user

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from django.contrib.auth.models import User
+from users.models import User
 from .serializers import RegisterSerializer, UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
